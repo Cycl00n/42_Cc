49bb254926c226091246a3ea748a30bc58b49826
Clément N <nicolas-clement1997@hotmail.fr>
2025-05-31 16:11:08 +0200

    Add files via upload

---
+++ Libft/Functions/ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clnicola <clnicola@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 15:57:16 by clnicola          #+#    #+#             */
+/*   Updated: 2025/05/31 16:09:22 by clnicola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+
+	i = 0;
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+	while (i < n)
+	{
+		if (ptr1[i] != ptr2[i])
+		{
+			return (ptr1[i] - ptr2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
