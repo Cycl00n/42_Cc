3d010f938004af26569f37f38a9bf31b59ce2e31
Clément N <nicolas-clement1997@hotmail.fr>
2025-06-05 16:37:38 +0200

    Add files via upload

---
+++ Libft/ft_strjoin.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clnicola <clnicola@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/05 15:59:18 by clnicola          #+#    #+#             */
+/*   Updated: 2025/06/05 16:17:36 by clnicola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i ++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		j ++;
+		i ++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*#include <stdio.h>
+int main()
+{
+	printf("%s", ft_strjoin("Hello ","world"));
+}*/
